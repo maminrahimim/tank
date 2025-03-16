4825a102d080c635d215e42b4b0c707755212763
Amin Majd <m.aminrahimi.m@gmail.com>
2025-03-16 23:18:09 +0330

    task 2

--- lib/presentation/screens/calculator.dart
+++ lib/presentation/screens/calculator.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class CalculatorScreen extends StatelessWidget {
+class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String input = '';
+  String output = '';
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +18,37 @@ class CalculatorScreen extends StatelessWidget {
         children: [
           Expanded(
             child: Container(
+              width: double.infinity,
               margin: const EdgeInsets.all(8),
-              alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.black87,
               ),
-              child: const TextField(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    input,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                  Text(
+                    output,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ],
+              ),
             ),
           ),
           const SizedBox(height: 8),
@@ -30,11 +62,11 @@ class CalculatorScreen extends StatelessWidget {
                   Expanded(
                     child: Column(
                       children: [
-                        cell(flex: 1, text: 'AC', color: Colors.orange),
-                        cell(flex: 1, text: '7', color: Colors.black87),
-                        cell(flex: 1, text: '4', color: Colors.black87),
-                        cell(flex: 1, text: '1', color: Colors.black87),
-                        cell(flex: 1, text: 'C', color: Colors.orange),
+                        _buildButton(flex: 1, text: '⌫', color: Colors.blueAccent),
+                        _buildButton(flex: 1, text: '7', color: Colors.black87),
+                        _buildButton(flex: 1, text: '4', color: Colors.black87),
+                        _buildButton(flex: 1, text: '1', color: Colors.black87),
+                        _buildButton(flex: 1, text: 'C', color: Colors.blueAccent),
                       ],
                     ),
                   ),
@@ -42,11 +74,11 @@ class CalculatorScreen extends StatelessWidget {
                   Expanded(
                     child: Column(
                       children: [
-                        cell(flex: 1, text: '/', color: Colors.orange),
-                        cell(flex: 1, text: '8', color: Colors.black87),
-                        cell(flex: 1, text: '5', color: Colors.black87),
-                        cell(flex: 1, text: '2', color: Colors.black87),
-                        cell(flex: 1, text: '0', color: Colors.black87),
+                        _buildButton(flex: 1, text: '/', color: Colors.blueAccent),
+                        _buildButton(flex: 1, text: '8', color: Colors.black87),
+                        _buildButton(flex: 1, text: '5', color: Colors.black87),
+                        _buildButton(flex: 1, text: '2', color: Colors.black87),
+                        _buildButton(flex: 1, text: '0', color: Colors.black87),
                       ],
                     ),
                   ),
@@ -54,11 +86,11 @@ class CalculatorScreen extends StatelessWidget {
                   Expanded(
                     child: Column(
                       children: [
-                        cell(flex: 1, text: '*', color: Colors.orange),
-                        cell(flex: 1, text: '9', color: Colors.black87),
-                        cell(flex: 1, text: '6', color: Colors.black87),
-                        cell(flex: 1, text: '3', color: Colors.black87),
-                        cell(flex: 1, text: '.', color: Colors.black87),
+                        _buildButton(flex: 1, text: '*', color: Colors.blueAccent),
+                        _buildButton(flex: 1, text: '9', color: Colors.black87),
+                        _buildButton(flex: 1, text: '6', color: Colors.black87),
+                        _buildButton(flex: 1, text: '3', color: Colors.black87),
+                        _buildButton(flex: 1, text: '.', color: Colors.black87),
                       ],
                     ),
                   ),
@@ -66,9 +98,9 @@ class CalculatorScreen extends StatelessWidget {
                   Expanded(
                     child: Column(
                       children: [
-                        cell(flex: 1, text: '-', color: Colors.orange),
-                        cell(flex: 2, text: '+', color: Colors.orange),
-                        cell(flex: 2, text: '=', color: Colors.orange),
+                        _buildButton(flex: 1, text: '-', color: Colors.blueAccent),
+                        _buildButton(flex: 2, text: '+', color: Colors.blueAccent),
+                        _buildButton(flex: 2, text: '=', color: Colors.blueAccent),
                       ],
                     ),
                   ),
@@ -81,21 +113,51 @@ class CalculatorScreen extends StatelessWidget {
     );
   }
 
-  Widget cell({required int flex, required String text, required Color color}) {
+  Widget _buildButton({required int flex, required String text, required Color color}) {
     return Expanded(
       flex: flex,
       child: GestureDetector(
-        onTap: () {},
+        onTap: () {
+          if (text == 'C') {
+            setState(() {
+              input = '';
+              output = '';
+            });
+            return;
+          }
+          if (text == '⌫') {
+            if (input.isNotEmpty) {
+              setState(() {
+                input = input.substring(0, input.length - 1);
+              });
+            }
+            return;
+          }
+          if (text == '=') {
+            ///
+            return;
+          }
+          if (input.isNotEmpty && '/*-+'.contains(input.substring(input.length - 1)) && '/*-+'.contains(text)) {
+            setState(() {
+              input = input.substring(0, input.length - 1) + text;
+            });
+            return;
+          }
+          if (input.isEmpty && '/*-+0'.contains(text)) {
+            return;
+          }
+          setState(() {input = input + text;});
+        },
         child: Container(
           margin: const EdgeInsets.only(bottom: 8),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(20),
             color: color,
           ),
           child: Center(
             child: Text(
               text,
-              style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 18),
             ),
           ),
         ),
